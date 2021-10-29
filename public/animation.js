6ef0f57a5043de506e08f03eb83d3177af3b3978
Hanne Mørkeseth <hanne@morkeseth.no>
2021-10-29 20:16:26 +0200

    update animation

--- public/animation.js
+++ public/animation.js
@@ -1,11 +1,11 @@
+
+
 function animateTo(element) {
-  if (!CSS.supports("scroll-behavior", "smooth")) {
   const startPosition = window.pageYOffset;
   const endPosition = element.getBoundingClientRect().top;
-  const diff = endPosition - startPosition;
   const duration = 300;
 
-  let startTime, requestId;
+  let startTime;
 
   function scroll(timestamp) {
     if (startTime===undefined) {
@@ -17,14 +17,21 @@ function animateTo(element) {
     window.scrollTo(0, startPosition + endPosition * percentMoved);
 
     if (timeMoved<duration) {
-      requestId = window.requestAnimationFrame(scroll);
-    } else {
-      window.cancelAnimationFrame(requestId);
+      window.requestAnimationFrame(scroll);
     }
-
   }
   window.requestAnimationFrame(scroll);
-
-
-  }
 }
+
+
+
+document.querySelectorAll(".scroll-link").forEach(el => {
+  if (CSS.supports("scroll-behavior", "smooth")) {
+    return;
+  }
+  el.addEventListener("click", () => {
+    const id = el.getAttribute("href");
+    const target = document.querySelector(id);
+    animateTo(target)
+  })
+})
